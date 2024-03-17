d054f05652aa3d2474ff5369337cbafb528ed647
DELL <yasilkhan124@gmail.com>
2024-03-17 14:36:15 +0530

    first commit

---
+++ lib/core/costant.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+double h = 0.0;
+double w = 0.0;
+
+const AssetImage logo = AssetImage('assets/2359094.jpg');
+const AssetImage profileImage = AssetImage('assets/mainphoto.png');
+String frondImage = "assets/mainphoto.png";
+// const String aboutContent = 
+//     "As an enthusiastic and self-motivated professional, I thrive in challenging situations, showcasing "
+//    "adaptability and maturity as a team player. \n       With a proven track record of reliability and responsibility',"
+//    "I bring a proactive approach to all tasks, whether collaborating with others or taking the initiative independently."
+//    "Known for excelling under pressure, I consistently adhere to strict deadlines, ensuring the successful completion of "
+//    "projects.\n   My dedication to delivering results makes me a valuable asset in any team environment.'";
+
+StateProvider self =StateProvider((ref) => "As an enthusiastic and self-motivated professional, I thrive in challenging situations, showcasing "
+   "adaptability and maturity as a team player. \n       With a proven track record of reliability and responsibility',"
+   "I bring a proactive approach to all tasks, whether collaborating with others or taking the initiative independently."
+   "Known for excelling under pressure, I consistently adhere to strict deadlines, ensuring the successful completion of "
+   "projects.\n   My dedication to delivering results makes me a valuable asset in any team environment.'");
---
+++ lib/screen/aboutUs.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:portfolio/core/colors.dart';
+import 'package:portfolio/core/costant.dart';
+import 'package:portfolio/core/utiles/responsiveLayout.dart';
+import 'package:responsive_ui_builder/widget_builders.dart';
+
+
+class AboutUs extends StatelessWidget {
+  const AboutUs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget.isSmallScreen(context)
+        ? SizedBox(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: w * 0.5,
+                  height: h * 1,
+                  color: Colors.white,
+                ),
+                Container(
+                  width: w * 0.5,
+                  height: h * 1,
+                  color: Colors.red,
+                )
+              ],
+            ),
+          )
+        : SizedBox(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: w * 0.5,
+                  height: h * 1,
+                  color: Colors.white,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          top: w * 0.08,
+                          left: w * 0.05,
+                          child: Container(
+                            height: h * 0.5,
+                            width: w * 0.3,
+                            color: Colors.red,
+                          )),
+                      Positioned(
+                          top: w * 0.1,
+                          left: w * 0.08,
+                          child: Container(
+                            height: h * 0.5,
+                            width: w * 0.3,
+                            color: Colors.blue,
+                          )),
+                      Positioned(
+                          top: w * 0.12,
+                          left: w * 0.1,
+                          child: Container(
+                            height: h * 0.5,
+                            width: w * 0.3,
+                            color: Colors.black,
+                          ))
+                    ],
+                  ),
+                ),
+                Container(
+                  width: w * 0.5,
+                  height: h * 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: h * 0.1,
+                        ),
+                        Consumer(
+                          builder: (context, ref, child) {
+                         var   aboutself = ref.read(self);
+                            return Text(
+                              aboutself,
+                              style: GoogleFonts.montserrat(
+                                  textStyle: TextStyle(
+                                      fontSize: 22, color: Colorses.text)),
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+          
+  }
+}
